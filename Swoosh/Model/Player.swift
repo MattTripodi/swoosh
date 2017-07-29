0c66daee45d10ffa58f603aa0e60c6d9a1b7aa55
Matt Tripodi <mtripodi1027@gmail.com>
2017-07-29 00:10:27 -0400

    IBActions setup

---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Matt Tripodi on 7/29/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+	var desiredLeague: String?
+	var selectedSkillLevel: String?
+}
